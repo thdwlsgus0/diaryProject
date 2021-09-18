23f3f16f56f50d4ed18adf990700f218c989368d
octofox1357 <octofox1357@gmail.com>
2021-09-18 17:18:45 +0900

    #1 typescript, apollo-server-express, typeorm, type-graphql 설정

---
+++ server/ormconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+   "type": "postgres",
+   "host": process.env.DB_HOST,
+   "port": process.env.DB_PORT,
+   "username": process.env.DB_USER,
+   "password": process.env.DB_PASS,
+   "database": process.env.DB_NAME,
+   "synchronize": true,
+   "logging": ["error"],
+   "entities": [
+      "src/entity/**/*.ts"
+   ],
+   "migrations": [
+      "src/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "entitiesDir": "server/src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
