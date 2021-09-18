2039e32809d839f6c424ce4526fddef50d0f2d86
octofox1357 <octofox1357@gmail.com>
2021-09-18 17:18:45 +0900

    #1 typescript, apollo-server-express, typeorm, type-graphql 설정

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
+
+(async () => {
+  const app = express();
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("express sever started");
+  });
+})();
