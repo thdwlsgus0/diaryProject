04eac6f75e6516b15b90720f6c0942bddade037e
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-11-18 20:43:19 +0900

    chore: 메인페이지 라우팅을 위해서 위치 변경(#36)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,16 @@
-import React from 'react';
-import { Header } from '@components/organisms/Header/Header';
-import { MainText } from '@components/organisms/MainText/MainText';
-import { createGlobalStyle } from 'styled-components';
+import React, { FC } from 'react';
 import styled from 'styled-components';
+import Main from '@components/templates/Main';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const GlobalStyle = createGlobalStyle`
-	body {
-		margin: 0;
-		padding: 0;
-	}
-
-	p {
-		margin: 0;
-	}
-`;
-
-const MainBodyContainer = styled.div`
-	&:not(:first-child) {
-		margin-top: 60px;
-	}
-
-	padding: 0 20px;
-	text-align: center;
-`;
-
-const MainLinkContainer = styled.a`
-	&:not(:first-child) {
-		margin-top: 100px;
-	}
-	
-	display: inline-block;
-	font-size: 30px;
-	font-weight: bold;
-	padding: 20px;
-	background-color: #59886b;
-	color: #fff;
-`;
-
-const App = () => {
+const App:FC = () => {
 	return (
 		<>
-			<GlobalStyle/>
-			<Header/>
-			<MainBodyContainer>
-				<MainText/>
-				<MainLinkContainer>서비스 소개</MainLinkContainer>
-			</MainBodyContainer>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Main}/>
+				</Switch>
+			</BrowserRouter>
 		</>
 	)
 }
---
+++ client/src/components/templates/Main.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Header } from '@components/organisms/Header/Header';
+import { MainText } from '@components/organisms/MainText/MainText';
+import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		margin: 0;
+		padding: 0;
+	}
+
+	p {
+		margin: 0;
+	}
+`;
+
+const MainBodyContainer = styled.div`
+	&:not(:first-child) {
+		margin-top: 60px;
+	}
+
+	padding: 0 20px;
+	text-align: center;
+`;
+
+const MainLinkContainer = styled.a`
+	&:not(:first-child) {
+		margin-top: 100px;
+	}
+	
+	display: inline-block;
+	font-size: 30px;
+	font-weight: bold;
+	padding: 20px;
+	background-color: #59886b;
+	color: #fff;
+`;
+
+const Main = () => {
+	return (
+		<>
+			<GlobalStyle/>
+			<Header/>
+			<MainBodyContainer>
+				<MainText/>
+				<MainLinkContainer>서비스 소개</MainLinkContainer>
+			</MainBodyContainer>
+		</>
+	)
+}
+
+export default Main;
